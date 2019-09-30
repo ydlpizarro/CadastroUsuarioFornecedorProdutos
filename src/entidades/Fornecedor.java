0875f66245c271c17da3bf7b19651bd8768796a1
Yohel <yohel.larrpizarro@gmail.com>
2019-09-30 15:48:37 -0300

    Fornecedor e Produto

--- src/entidades/Fornecedor.java
+++ src/entidades/Fornecedor.java
@@ -7,8 +7,57 @@ package entidades;
 
 /**
  *
- * @author lab01a
+ * @author Gonosuke
  */
 public class Fornecedor {
+    private String nome;
+    private Endereco endereco;
+    private String cnpj;
+    private Usuario usuario;
+    private String tipoProduto;
+
+    public String getTipoProduto() {
+        return tipoProduto;
+    }
+
+    public void setTipoProduto(String tipoProduto) {
+        this.tipoProduto = tipoProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
     
+    @Override
+    public String toString(){
+        return nome;
+    }
 }
--- src/entidades/Produto.java
+++ src/entidades/Produto.java
@@ -7,8 +7,64 @@ package entidades;
 
 /**
  *
- * @author lab01a
+ * @author Gonosuke
  */
 public class Produto {
+    private String nome;
+    private Fornecedor fornecedor;
+    private int quantidade;
+    private double valorUnitario;
+    private String tipo;
+    private String codigoProduto;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public void setCodigoProduto(String codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }  
+    
+    
     
 }
